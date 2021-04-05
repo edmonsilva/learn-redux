034d8abbaa16419d10fdf02cebdba2ef9e1d1bdf
Edmon <edmon.silva@gmail.com>
2021-04-05 17:14:47 -0300

    ajustes para rodar npm

--- learn-redux/src/index.js
+++ learn-redux/src/index.js
@@ -44,8 +44,3 @@ store.subscribe(() => console.log(store.getState()));
 store.dispatch(increment());
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
