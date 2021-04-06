1fe162ff4810d0ebea0a8c8e1b0b96156cff6204
Edmon <edmon.silva@gmail.com>
2021-04-06 17:35:00 -0300

    exercicio completo

--- learn-redux/src/App.js
+++ learn-redux/src/App.js
@@ -1,10 +1,19 @@
 import React from "react"
-// import ReactDOM from "react-dom"
+// import ReactDOM from "react-dom";
+import {useSelector, useDispatch} from "react-redux";
+import {decrement, increment} from "./actions"
 
 function App() {
+  const counter = useSelector(state => state.counter);
+  const isLogged = useSelector(state => state.isLogged);
+  const dispatch = useDispatch();
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <h1>Counter: {counter}</h1>
+      <button onClick={() => dispatch(increment(5))}>+</button>
+      <button onClick={() => dispatch(decrement(5))}>-</button>
+
+      {isLogged ? <h3>Classified Info!</h3> : ""}
     </div>
   );
 }
--- learn-redux/src/index.js
+++ learn-redux/src/index.js
@@ -3,45 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import {createStore} from "redux";
+import allReducers from "./reducers";
+import {Provider} from "react-redux";
 
-// Store
+const store = createStore(allReducers);
 
-
-
-// Actions
-
-const increment = () => {
-  return {
-    type: "INCREMENT"
-  };
-}
-
-const decrement = () => {
-  return {
-    type: "DECREMENT"
-  };
-}
-
-// Reducer
-
-const counter = (state = 0, action) => {
-  switch(action.type) {
-    case "INCREMENT":
-      return state + 1;
-    case "DECREMENT":
-      return state - 1;
-    default:
-      return state;
-  };
-}
-
-let store = createStore(counter);
-store.subscribe(() => console.log(store.getState()));
-
-// Dispatch
-
-store.dispatch(increment());
-store.dispatch(increment());
-store.dispatch(decrement());
-
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>, 
+  document.getElementById('root'));
